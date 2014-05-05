c8f56f74609a92b0031184e6ddb727fddebe6bea
Oliver Klee <typo3-coding@oliverklee.de>
2014-05-05 13:05:21 +0200

    [CLEANUP] Clean up and structure the HTML.

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Insecurity</title>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Insecurity</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+		<header>
+			<h1>Insecurity</h1>
+			<nav>
+			</nav>
+		</header>
 
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-<h1>Insecurity</h1>
+		<main>
+		</main>
 
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-</body>
+		<footer>
+			<script src="js/jquery.min.js"></script>
+			<script src="js/bootstrap.min.js"></script>
+		</footer>
+	</body>
 </html>
