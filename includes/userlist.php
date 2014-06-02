6f7e23fe775a2a9e2ce6980334132aa62529d5b7
Oliver Klee <typo3-coding@oliverklee.de>
2014-06-02 07:59:46 +0200

    [FEATURE] Add a full-text search for users.

--- includes/userlist.php
+++ includes/userlist.php
@@ -1,4 +1,13 @@
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+	<div class="navbar-collapse collapse">
+		<form class="navbar-form navbar-right" role="form" action="<?= $_SERVER['REQUEST_URI'] ?>" method="post">
+			<div class="form-group">
+				<input name="search_term" type="text" placeholder="Search term" class="form-control" value="<?= isset($_REQUEST['search_term']) ? $_REQUEST['search_term'] : '' ?>">
+			</div>
+			<button type="submit" class="btn btn-success">Search</button>
+		</form>
+	</div>
+
 	<div class="table-responsive">
 		<table class="table table-striped">
 			<thead>
@@ -15,7 +24,13 @@
 
 <?php
 $databaseConnection = getDatabaseConnection();
-$result = $databaseConnection->query('SELECT * FROM insecurity_users');
+if ($_REQUEST['search_term']) {
+	$searchTerm = $_REQUEST['search_term'];
+	$where = 'WHERE name LIKE "%' . $searchTerm . '%" OR email LIKE "%' . $searchTerm . '%"';
+} else {
+	$where = '';
+}
+$result = $databaseConnection->query('SELECT * FROM insecurity_users ' . $where);
 
 /** @var $user string[] */
 while (($user = $result->fetch_assoc())) {
