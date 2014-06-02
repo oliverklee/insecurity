774d24bb11f013ca4aeeceb03cc3488ed6bb15cf
Oliver Klee <typo3-coding@oliverklee.de>
2014-06-02 12:20:17 +0200

    [BUGFIX] Improve the navigation bar styling.

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,6 +1,4 @@
-<div class="header">
-	<ul class="nav nav-pills pull-left">
-		<li><a href="index.php">Home</a></li>
-		<li><a href="index.php?page=userlist.php">Users</a></li>
-	</ul>
-</div>
+<ul class="nav nav-tabs">
+	<li><a href="index.php">Home</a></li>
+	<li><a href="index.php?page=userlist.php">Users</a></li>
+</ul>
