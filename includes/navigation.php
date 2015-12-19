02a8891193328be94b0876ea36faa51f32421949
Oliver Klee <typo3-coding@oliverklee.de>
2015-12-19 15:56:58 +0100

    [FEATURE] Always display the login status

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,13 +1,17 @@
 <ul class="nav nav-tabs">
     <li><a href="index.php">Home</a></li>
     <li><a href="index.php?page=userlist.php">Users</a></li>
-    <li><a href="index.php?page=login.php">Login</a></li>
+    <li role="separator" class="divider"></li>
     <?php
     if (isLoggedIn()) {
         ?>
         <li><a href="index.php?page=logout.php">Logout</a></li>
         <?php
+    } else {
+        ?>
+        <li><a href="index.php?page=login.php">Login</a></li>
 
+        <?php
     }
     ?>
 </ul>
