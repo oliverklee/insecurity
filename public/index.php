cbac7f8fd8aae7099223455243d3c932007eeca3
Oliver Klee <typo3-coding@oliverklee.de>
2015-12-26 13:12:46 +0100

    [TASK] Add timestamp URL parameters to the JS and CSS links
    This will allow us to circument caches for new versions of the
    libraries.

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Insecurity</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css?201512261312" rel="stylesheet">
 </head>
 <body>
 <header>
@@ -30,8 +30,8 @@
 </main>
 
 <footer>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.min.js?201512261312"></script>
+    <script src="js/bootstrap.min.js?201512261312"></script>
 </footer>
 </body>
 </html>
